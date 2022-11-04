b31f584d096f80998f79892b91d7462cc5191ff9
Melvin <maheshdevarapally@imacbook-pro.lan>
2022-11-03 20:00:15 -0500

    first commit

---
+++ src/data/data.tsx
@@ -0,0 +1,373 @@
+import {
+  AcademicCapIcon,
+  CalendarIcon,
+  DownloadIcon,
+  FlagIcon,
+  MapIcon,
+  OfficeBuildingIcon,
+  SparklesIcon,
+} from '@heroicons/react/outline';
+
+import GithubIcon from '../components/Icon/GithubIcon';
+import InstagramIcon from '../components/Icon/InstagramIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+// import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+import TwitterIcon from '../components/Icon/TwitterIcon';
+import heroImage from '../images/header-background.webp';
+// import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+// import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+// import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+// import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+// import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+// import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+// import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+// import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+// import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+// import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import profilepic from '../images/dp.jpg';
+import testimonialImage from '../images/testimonial.webp';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: "Mahesh's Resume",
+  description: 'My sample resume build with react template',
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  // Testimonials: 'testimonials',
+} as const;
+
+export type SectionId = typeof SectionId[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `I'm Mahesh Devarapally.`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        A new <strong className="text-stone-100">Computer Science</strong> graduate, currently looking for{' '}
+        <strong className="text-stone-100">Software developer</strong> position.
+      </p>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        In my free time time, you can catch me reading a book or doing some coding.
+      </p>
+    </>
+  ),
+  actions: [
+    {
+      href: 'src/data/Resume.pdf',
+      text: 'Resume',
+      primary: true,
+      Icon: DownloadIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `I'm an Computer enthusiast, want to build my career in software development`,
+  aboutItems: [
+    {label: 'Location', text: 'Overland park, KS', Icon: MapIcon},
+    {label: 'Age', text: '27', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Indian', Icon: FlagIcon},
+    {label: 'Interests', text: '8 ball pool, reading books ', Icon: SparklesIcon},
+    {label: 'Study', text: 'Universiy of Central Missouri', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Ex - Amazon .Inc', Icon: OfficeBuildingIcon},
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  // {
+  //   name: 'Spoken languages',
+  //   skills: [
+  //     {
+  //       name: 'English',
+  //       level: 9,
+  //     },
+  //     {
+  //       name: 'Telugu',
+  //       level: 10,
+  //     },
+  //     {
+  //       name: 'Hindi',
+  //       level: 9,
+  //     },
+  //   ],
+  // },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'HTML',
+        level: 7,
+      },
+      {
+        name: 'CSS',
+        level: 6,
+      },
+      {
+        name: 'JavaScript',
+        level: 8,
+      },
+      {
+        name: 'React',
+        level: 5,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'Python',
+        level: 6,
+      },
+      {
+        name: 'Java',
+        level: 7,
+      },
+      {
+        name: 'JavaScript',
+        level: 4,
+      },
+    ],
+  },
+  // {
+  //   name: 'Mobile development',
+  //   skills: [
+  //     {
+  //       name: 'React Native',
+  //       level: 9,
+  //     },
+  //     {
+  //       name: 'Flutter',
+  //       level: 4,
+  //     },
+  //     {
+  //       name: 'Swift',
+  //       level: 3,
+  //     },
+  //   ],
+  // },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'To do app',
+    description: 'Simple to do app using react',
+    url: 'https://maheshdeva.github.io/todo.github.io/',
+    image: porfolioImage2,
+  },
+  // {
+  //   title: 'Project title 2',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage2,
+  // },
+  // {
+  //   title: 'Project title 3',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage3,
+  // },
+  // {
+  //   title: 'Project title 4',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage4,
+  // },
+  // {
+  //   title: 'Project title 5',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage5,
+  // },
+  // {
+  //   title: 'Project title 6',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage6,
+  // },
+  // {
+  //   title: 'Project title 7',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage7,
+  // },
+  // {
+  //   title: 'Project title 8',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage8,
+  // },
+  // {
+  //   title: 'Project title 9',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage9,
+  // },
+  // {
+  //   title: 'Project title 10',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage10,
+  // },
+  // {
+  //   title: 'Project title 11',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://timbaker.me',
+  //   image: porfolioImage11,
+  // },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: 'December 2022',
+    location: 'University of Central Missouri',
+    title: 'Masters in Computer Science',
+    content: <p>Learned about Computer Science fundametals and methods in software development.</p>,
+  },
+  {
+    date: 'May 2017',
+    location: 'VNR VJIET, Hyderabad',
+    title: 'Bachelors of Technology',
+    content: <p>In, Electronics and Communications Engineering.</p>,
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'May 2018 - August 2020',
+    location: 'Amazon Inc',
+    title: 'Data Analyst',
+    content: (
+      <p>
+        Training Artificial Intelligence to get accurate results in deciding the stowing using machine learning
+        algorithms using python Algorithms
+      </p>
+    ),
+  },
+  // {
+  //   date: 'March 2007 - February 2010',
+  //   location: 'Garage Startup Studio',
+  //   title: 'Junior bug fixer',
+  //   content: (
+  //     <p>
+  //       Describe work, special projects, notable achievements, what technologies you have been working with, and
+  //       anything else that would be useful for an employer to know.
+  //     </p>
+  //   ),
+  // },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    // {
+    //   name: 'John Doe',
+    //   text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+    // },
+    // {
+    //   name: 'Jane Doe',
+    //   text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    // },
+    // {
+    //   name: 'Someone else',
+    //   text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+    // },
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Get in touch.',
+  // description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'devarapallymahesh95@gmail.com',
+      href: 'mailto:devarapallymahesh95@gmail.com',
+    },
+    {
+      type: ContactType.Location,
+      text: 'Overland Park, Kansas, USA',
+      href: 'https://www.google.ca/maps/place/Overland+Park,+KS/@38.905672,-94.6843604,11z/data=!3m1!4b1!4m13!1m7!3m6!1s0x87c08ce427cf3515:0xdc77777b70c31e05!2sKansas+City,+KS!3b1!8m2!3d39.1155314!4d-94.6267873!3m4!1s0x87c09551a72638d3:0x24ce4feb3844f9c8!8m2!3d38.9822306!4d-94.6707344',
+    },
+    {
+      type: ContactType.Instagram,
+      text: '@mxhdeva',
+      href: 'https://www.instagram.com/mxhdeva/',
+    },
+    {
+      type: ContactType.Github,
+      text: 'maheshdeva',
+      href: 'https://github.com/maheshdeva',
+    },
+  ],
+  description: '',
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/maheshdeva'},
+  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/mxhdev'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/mxhdeva'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/MadevaRap'},
+];
